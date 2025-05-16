d1191cd01f19790f0ec69ccb0dbf677c8cde2302
Diana Lyvytska <diana.lyvytska@yalantis.net>
2025-05-17 00:44:17 +0200

    add html and fix cron

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,15 @@ startEmailScheduler();
 const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"));
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../public")));
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api", weatherRoutes);
 app.use("/api", subscriptionRoutes);
 
 app.get("/", (_req, res) => {
-  res.send("Check /api-docs");
+  res.send("Check <a href='/api-docs'>/api-docs</a> or <a href='/subscribe.html'>/subscribe.html</a>");
 });
 
 app.listen(port, () => {
